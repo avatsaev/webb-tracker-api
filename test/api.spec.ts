d39e4d67261e8a9d2c5c245aad3019e2a9c711d0
Frederic Woelffel <hello@fwoelffel.me>
2022-01-03 02:05:37 +0100

    test: basic e2e test

---
+++ test/api.spec.ts
@@ -0,0 +1,97 @@
+import { FastifyInstance, LightMyRequestResponse } from "fastify";
+import { Page } from "puppeteer";
+import { getServer } from "../src/server";
+import { getBrowser, getWebbPage } from "../src/helpers/puppetteer";
+
+describe("JWST API", () => {
+    let page: Page;
+    let server: FastifyInstance;
+
+    beforeAll(async () => {
+        page = await getBrowser().then(getWebbPage);
+        server = await getServer(page);
+    });
+
+    describe("/", () => {
+        describe("GET /", () => {
+            it("should respond with a HTTP 200 status", () =>
+                server
+                    .inject({ method: "GET", url: "/" })
+                    .then(({ statusCode }) => expect(statusCode).toBe(200)));
+        });
+    });
+
+    describe("/track", () => {
+        describe("GET /track", () => {
+            let response: LightMyRequestResponse;
+            beforeAll(async () => {
+                response = await server.inject({
+                    method: "GET",
+                    url: "/track"
+                });
+            });
+            it("should respond with a HTTP 200 status", () =>
+                expect(response.statusCode).toBe(200));
+            it('should respond with a valid "distanceEarthKm" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "distanceEarthKm",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "launchElapsedTime" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "launchElapsedTime",
+                    expect.any(String)
+                ));
+            it('should respond with a valid "distanceL2Km" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "distanceL2Km",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "percentageCompleted" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "percentageCompleted",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "speedKmS" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "speedKmS",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "deploymentImgURL" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "deploymentImgURL",
+                    expect.any(String)
+                ));
+            it('should respond with a valid "currentDeploymentStep" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "currentDeploymentStep",
+                    expect.any(String)
+                ));
+            it('should respond with a valid "tempC.tempWarmSide1C" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "tempC.tempWarmSide1C",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "tempC.tempWarmSide2C" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "tempC.tempWarmSide2C",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "tempC.tempCoolSide1C" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "tempC.tempCoolSide1C",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "tempC.tempCoolSide2C" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "tempC.tempCoolSide2C",
+                    expect.any(Number)
+                ));
+            it('should respond with a valid "timestamp" value', () =>
+                expect(response.json()).toHaveProperty(
+                    "timestamp",
+                    expect.any(String)
+                ));
+        });
+    });
+});
