73b58ecb7ed53a889a371664cd2e70ed18890d6a
Aslan Vatsaev <a.vatsaev@gmail.com>
2022-01-14 12:36:06 +0100

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,19 @@ import { Page } from "puppeteer";
 import Fastify, { FastifyInstance } from "fastify";
 import { scrapWebbTrackingData } from "./webb-tracker.service";
 import { randomUUID } from "crypto";
+import FastifyCors from "fastify-cors";
+
 
 export async function getServer(page: Page): Promise<FastifyInstance> {
     const instance = Fastify({
         logger: true
     });
+
+    await instance.register(FastifyCors, {
+      origin: "*",
+      methods: ["GET"],
+    });
+
     instance.get("/", {}, () =>
         Promise.resolve({
             status: "ok",
@@ -14,6 +22,9 @@ export async function getServer(page: Page): Promise<FastifyInstance> {
             serverID: randomUUID()
         })
     );
-    instance.get("/track", () => scrapWebbTrackingData(page));
+    instance.get("/track", (req) => {
+      console.log("TRACK", req.ip);
+      return scrapWebbTrackingData(page)
+    });
     return instance;
 }
